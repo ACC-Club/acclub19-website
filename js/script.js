e625fbad6fd4e7cf37689a1ff66cc5d2b96691b3
Andrew Roembke <aroembke21@gmail.com>
2019-02-20 15:45:48 -0500

    initial commit of the redo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-$(document).ready(function() {
-  $('#slides').superslides({
-    animation: 'fade',
-    play: 5000,
-    pagination: false
-  });
-
-
+// Can also be used with $(document).ready(function()
+$(document).ready(function(){
+    $('#f1').flexslider({
+        animation: "slide",
+        rtl: true,
+        start: function(slider){
+            $('body').removeClass('loading');
+        }
+    });
+    $('#f2').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        pausePlay: true,
+        mousewheel: true,
+        rtl: true,
+        asNavFor:'.flexslider'
+    });
 });
